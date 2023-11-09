133c0b4e76ff37b7af7aa1dd4d0523569e7daa12
Massaki <enzomassaki@hotmail.com>
2023-11-08 23:44:35 -0300

    adding -l to read the lines the of the file

--- build.zig
+++
@@ -1,8 +0,0 @@
-const Builder = @import("std").build.Builder;
-
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("ccwc", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.install();
-}
--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,13 @@ pub fn main() !void {
                 return;
             }
             try countBytes(filename.?);
+        } else if (std.mem.eql(u8, arg, "-l")) {
+            const filename = args.next();
+            if (filename == null) {
+                std.debug.print("Error: Missing filename argument. \n", .{});
+                return;
+            }
+            try countLines(filename.?);
         } else {
             std.debug.print("Error: Unexpected argument '{s}'.\n", .{arg});
             return;
@@ -27,4 +34,17 @@ fn countBytes(filename: []const u8) !void {
     const fileSize = fileInfo.size;
 
     std.debug.print("{d} {s}\n", .{ fileSize, filename });
+}
+
+fn countLines(filename: []const u8) !void {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+    
+    var lineCount: usize = 0;
+    var buffer: [4096]u8 = undefined;
+    while (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |_| {
+        lineCount += 1;
+    }
+    
+    std.debug.print("{d} {s}\n", .{ lineCount,filename });
 }
